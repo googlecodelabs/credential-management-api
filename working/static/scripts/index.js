71ccf7f88abbf9c63499cf04c90e70b585eb5da2
Eiji Kitamura <agektmr@google.com>
2016-05-02 13:49:25 +0900

    merged registration fix

--- working/static/scripts/index.js
+++ working/static/scripts/index.js
@@ -1,38 +1,46 @@
-app.onReg = function() {
-  var form = document.querySelector('#regForm');
-  fetch('/register', {
-    method: 'POST',
-    credentials: 'include',
-    body: new FormData(form)
-  })
-  .then(function(res) {
-    if (res.status === 200) {
-      if (navigator.credentials) {
-        var cred = new PasswordCredential(form);
-        navigator.credentials.store(cred)
-        .then(function() {
-          location.href = '/main?quote=You are registered';
-        });
-      } else {
-        location.href = '/main?quote=You are registered';
-      }
-    } else {
-      app.fire('show-toast', {
-        text: 'Registration failed'
-      });
-    }
-  }, function() {
-    app.fire('show-toast', {
-      text: 'Registration failed'
-    });
-  });
-};
+app.addEventListener('dom-change', function() {
+  var regForm = document.querySelector('#regForm');
+  if (regForm) {
+    regForm.addEventListener('submit', function(e) {
+      e.preventDefault();
 
-app.onSignIn = function() {
-  autoSignIn(false)
-  .then(function() {
-    location.href = '/main';
-  }, function() {
-    location.href = '/signin';
-  });
-};
+      fetch('/register', {
+        method: 'POST',
+        credentials: 'include',
+        body: new FormData(regForm)
+      }).then(function(res) {
+        if (res.status === 200) {
+          if (navigator.credentials) {
+            var cred = new PasswordCredential(regForm);
+            navigator.credentials.store(cred)
+            .then(function() {
+              location.href = '/main?quote=You are registered';
+            });
+          } else {
+            location.href = '/main?quote=You are registered';
+          }
+        } else {
+          app.fire('show-toast', {
+            text: 'Registration failed'
+          });
+        }
+      }, function() {
+        app.fire('show-toast', {
+          text: 'Registration failed'
+        });
+      });
+    });
+  };
+
+  var signin = document.querySelector('#signin');
+  if (signin) {
+    signin.addEventListener('click', function() {
+      autoSignIn(false)
+      .then(function() {
+        location.href = '/main';
+      }, function() {
+        location.href = '/signin';
+      });
+    });
+  }
+});

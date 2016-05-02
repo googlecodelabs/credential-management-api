20c16ef17b6a1b66e1bc23cec97154a4f64608f4
Eiji Kitamura <agektmr@google.com>
2016-05-02 14:02:39 +0900

    use addEventListener for signin form submission

--- working/static/scripts/signin.js
+++ working/static/scripts/signin.js
@@ -1,29 +1,34 @@
-app.onPwSignIn = function() {
-  var form = document.querySelector('#form');
-  fetch('/auth/password', {
-    method: 'POST',
-    credentials: 'include',
-    body: new FormData(form)
-  })
-  .then(function(res) {
-    if (res.status === 200) {
-      if (navigator.credentials) {
-        var cred = new PasswordCredential(form);
-        navigator.credentials.store(cred)
-        .then(function() {
-          location.href = '/main?quote=You are signed in';
+app.addEventListener('dom-change', function() {
+  var form = document.querySelector('form');
+  if (form) {
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      fetch('/auth/password', {
+        method: 'POST',
+        credentials: 'include',
+        body: new FormData(form)
+      }).then(function(res) {
+        if (res.status === 200) {
+          if (navigator.credentials) {
+            var cred = new PasswordCredential(form);
+            navigator.credentials.store(cred)
+            .then(function() {
+              location.href = '/main?quote=You are signed in';
+            });
+          } else {
+            location.href = '/main?quote=You are signed in';
+          }
+        } else {
+          app.fire('show-toast', {
+            text: 'Authentication failed'
+          });
+        }
+      }, function() {
+        app.fire('show-toast', {
+          text: 'Authentication failed'
         });
-      } else {
-        location.href = '/main?quote=You are signed in';
-      }
-    } else {
-      app.fire('show-toast', {
-        text: 'Authentication failed'
       });
-    }
-  }, function() {
-    app.fire('show-toast', {
-      text: 'Authentication failed'
     });
-  });
-};
+  }
+});

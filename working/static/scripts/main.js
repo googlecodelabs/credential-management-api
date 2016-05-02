c69568215f76ecdb3aa645c205631f437e3e11c7
Eiji Kitamura <agektmr@google.com>
2016-05-02 14:15:03 +0900

    changed signout and unregister

--- working/static/scripts/main.js
+++ working/static/scripts/main.js
@@ -1,42 +1,51 @@
-app.onSignOut = function(e) {
-  e.preventDefault();
-  if (navigator.credentials) {
-    navigator.credentials.requireUserMediation()
-    .then(function() {
-      location.href = '/?quote=You are signed out';
+app.addEventListener('dom-change', function() {
+  var unregister = document.querySelector('#unregister');
+  if (unregister) {
+    unregister.addEventListener('click', function() {
+      var csrf_token = document.querySelector('#csrf_token').value;
+      var form = new FormData();
+      form.append('csrf_token', csrf_token);
+
+      fetch('/unregister', {
+        method: 'POST',
+        credentials: 'include',
+        body: form
+      }).then(function(res) {
+        if (res.status === 200) {
+          if (navigator.credentials) {
+            navigator.credentials.requireUserMediation()
+            .then(function() {
+              location.href = '/?quote=You are signed out';
+            });
+          } else {
+            location.href = '/?quote=You are unregistered';
+          }
+        } else {
+          app.fire('show-toast', {
+            text: 'Unregister failed'
+          });
+        }
+      }, function() {
+        app.fire('show-toast', {
+          text: 'Unregister failed'
+        });
+      });
     });
-  } else {
-    location.href = '/?quote=You are signed out';
   }
-};
 
-app.onUnreg = function() {
-  var csrf_token = document.querySelector('#csrf_token').value;
-  var form = new FormData();
-  form.append('csrf_token', csrf_token);
+  var signout = document.querySelector('#signout');
+  if (signout) {
+    signout.addEventListener('click', function(e) {
+      e.preventDefault();
 
-  fetch('/unregister', {
-    method: 'POST',
-    credentials: 'include',
-    body: form
-  }).then(function(res) {
-    if (res.status === 200) {
       if (navigator.credentials) {
         navigator.credentials.requireUserMediation()
         .then(function() {
           location.href = '/?quote=You are signed out';
         });
       } else {
-        location.href = '/?quote=You are unregistered';
+        location.href = '/?quote=You are signed out';
       }
-    } else {
-      app.fire('show-toast', {
-        text: 'Unregister failed'
-      });
-    }
-  }, function() {
-    app.fire('show-toast', {
-      text: 'Unregister failed'
     });
-  });
-};
+  }
+});

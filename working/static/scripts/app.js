69e073850d6c57a9926406e3a357dc38271823bc
Eiji Kitamura <agektmr@google.com>
2016-05-10 10:11:07 +0900

    merged

--- working/static/scripts/app.js
+++ working/static/scripts/app.js
@@ -20,69 +20,10 @@
   handled using regular APIs so you don't have to learn about it.
  */
 var app = document.querySelector('#app');
-// `selected` is used to show a portion of our page
-app.selected = 0;
 // Set an event listener to show a toast. (Polymer)
 app.listeners = {
   'show-toast': 'showToast'
 };
-
-app.addEventListener('dom-change', function() {
-  /**
-   * When google sign-in button is pressed.
-   * @return {void}
-   */
-  var gsignin = document.querySelector('#gsignin');
-  if (gsignin) {
-    gsignin.addEventListener('click', function() {
-      gSignIn()
-      .then(function(googleUser) {
-        // Now user is successfully authenticated with Google.
-        // Send ID Token to the server to authenticate with our server.
-        var form = new FormData();
-        form.append('id_token', googleUser.getAuthResponse().id_token);
-        form.append('csrf_token', document.querySelector('#csrf_token').value);
-
-        return fetch('/auth/google', {
-          method: 'POST',
-          credentials: 'include',
-          body: form
-        }).then(function(res) {
-          if (res.status === 200) {
-            if (navigator.credentials) {
-              var profile = googleUser.getBasicProfile();
-              var cred = new FederatedCredential({
-                id: profile.getEmail(),
-                name: profile.getName(),
-                iconURL: profile.getImageUrl(),
-                provider: GOOGLE_SIGNIN
-              });
-              return navigator.credentials.store(cred);
-            } else {
-              return Promise.resolve();
-            }
-          } else {
-            return Promise.reject();
-          }
-        });
-      }).then(function() {
-        location.href = '/main?quote=You are signed in with Google SignIn';
-      }, function() {
-        app.fire('show-toast', {
-          text: 'Google Sign-In failed'
-        });
-      });
-    });
-  }
-
-  var url = new URL(location.href);
-  var params = new URLSearchParams(url.search.slice(1));
-  if (params.get('quote')) {
-    app.fire('show-toast', {
-      text: params.get('quote')
-    });
-  }
-});
 
 /**
  * Polymer event handler to show a toast.
@@ -93,3 +34,59 @@ app.showToast = function(e) {
   this.$.toast.text = e.detail.text;
   this.$.toast.show();
 };
+
+app.addEventListener('dom-change', function() {
+  var url = new URL(location.href);
+  var params = new URLSearchParams(url.search.slice(1));
+  if (params.get('quote')) {
+    app.fire('show-toast', {
+      text: params.get('quote')
+    });
+  }
+});
+
+/* When google sign-in button is pressed.
+* @return {void}
+*/
+var gsignin = document.querySelector('#gsignin');
+if (gsignin) {
+  gsignin.addEventListener('click', function() {
+    gSignIn()
+    .then(function(googleUser) {
+      // Now user is successfully authenticated with Google.
+      // Send ID Token to the server to authenticate with our server.
+      var form = new FormData();
+      form.append('id_token', googleUser.getAuthResponse().id_token);
+      form.append('csrf_token', document.querySelector('#csrf_token').value);
+
+      return fetch('/auth/google', {
+        method: 'POST',
+        credentials: 'include',
+        body: form
+      }).then(function(res) {
+        if (res.status === 200) {
+          if (navigator.credentials) {
+            var profile = googleUser.getBasicProfile();
+            var cred = new FederatedCredential({
+              id: profile.getEmail(),
+              name: profile.getName(),
+              iconURL: profile.getImageUrl(),
+              provider: GOOGLE_SIGNIN
+            });
+            return navigator.credentials.store(cred);
+          } else {
+            return Promise.resolve();
+          }
+        } else {
+          return Promise.reject();
+        }
+      });
+    }).then(function() {
+      location.href = '/main?quote=You are signed in with Google SignIn';
+    }, function() {
+      app.fire('show-toast', {
+        text: 'Google Sign-In failed'
+      });
+    });
+  });
+}
